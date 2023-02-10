cf3fd553081aa6001ad5c2d9c0854656ce332cef
ExecBeni3 <chillwallop@gmail.com>
2023-02-10 16:36:17 +0100

    Juicer fixing 1.0

--- src/main/java/com/yuanno/block_clover/blocks/JuicerBlock.java
+++ src/main/java/com/yuanno/block_clover/blocks/JuicerBlock.java
@@ -1,12 +1,12 @@
 package com.yuanno.block_clover.blocks;
 
-import com.yuanno.block_clover.blocks.tileentities.JuicerBlockTileEntity;
 import com.yuanno.block_clover.blocks.containers.JuicerContainer;
+import com.yuanno.block_clover.blocks.tileentities.JuicerBlockTileEntity;
 import com.yuanno.block_clover.init.ModTileEntities;
-import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ContainerBlock;
+import net.minecraft.block.HorizontalBlock;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
 import net.minecraft.entity.player.PlayerEntity;
@@ -15,13 +15,13 @@ import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.inventory.container.Container;
 import net.minecraft.inventory.container.INamedContainerProvider;
-import net.minecraft.tileentity.AbstractFurnaceTileEntity;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.StateContainer;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Hand;
+import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
-import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.IBlockReader;
@@ -31,8 +31,11 @@ import net.minecraftforge.fml.network.NetworkHooks;
 import javax.annotation.Nullable;
 
 public class JuicerBlock extends ContainerBlock {
+    public static final DirectionProperty FACING = HorizontalBlock.FACING;
+
     public JuicerBlock() {
-        super(Properties.of(Material.STONE, MaterialColor.COLOR_LIGHT_GREEN).requiresCorrectToolForDrops().strength(4F).noOcclusion());
+        super(Properties.of(Material.STONE, MaterialColor.COLOR_LIGHT_GREEN).requiresCorrectToolForDrops().strength(4F));
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
     }
 
     @Override
@@ -94,4 +97,24 @@ public class JuicerBlock extends ContainerBlock {
             super.onRemove(state, world, pos, newstate, sim);
         }
     }
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    public BlockState rotate(BlockState state, Rotation rot) {
+        return state.setValue(FACING, rot.rotate(state.getValue(FACING)));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> stateContainer) {
+        stateContainer.add(FACING);
+    }
+
+    @Override
+    public BlockState mirror(BlockState state, Mirror mirror) {
+        return state.rotate(mirror.getRotation(state.getValue(FACING)));
+    }
+
 }
--- src/main/java/com/yuanno/block_clover/entities/VolcanoMonsterEntity.java
+++ src/main/java/com/yuanno/block_clover/entities/VolcanoMonsterEntity.java
@@ -90,4 +90,5 @@ public class VolcanoMonsterEntity extends BCentity
 
     }
 
+
 }

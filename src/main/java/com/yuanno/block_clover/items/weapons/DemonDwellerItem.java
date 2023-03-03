ce6ab5b7315749d4122b7711b8e2d2b8612ad387
shibeo <ziroxismail@gmail.com>
2023-03-03 01:02:52 +0100

    Working on sword attribute Mostly working on blocking/absorbing magic

--- src/main/java/com/yuanno/block_clover/items/weapons/DemonDwellerItem.java
+++ src/main/java/com/yuanno/block_clover/items/weapons/DemonDwellerItem.java
@@ -3,18 +3,28 @@ package com.yuanno.block_clover.items.weapons;
 import com.yuanno.block_clover.init.ModItemGroup;
 import com.yuanno.block_clover.init.ModTiers;
 import com.yuanno.block_clover.items.MagicWeapon;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUseContext;
 import net.minecraft.item.UseAction;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
 
 public class DemonDwellerItem extends MagicWeapon {
 
     public DemonDwellerItem() {
         super(ModTiers.WEAPON, 7, 1f, new Item.Properties().tab(ModItemGroup.BLOCK_CLOVER_WEAPONS).stacksTo(1));
     }
-
+    // just use chatgpt
     @Override
-    public UseAction getUseAnimation(ItemStack p_77661_1_) {
-        return UseAction.BLOCK;
+    public boolean canHarvestBlock(BlockState blockIn) {
+        return blockIn.getBlock().getHarvestTool(blockIn) == "sword";
     }
 }
---
+++ src/main/java/com/yuanno/block_clover/spells/sword/OriginalSlashesAbility.java
@@ -0,0 +1,41 @@
+package com.yuanno.block_clover.spells.sword;
+
+import com.yuanno.block_clover.api.ability.AbilityCategories;
+import com.yuanno.block_clover.api.ability.AbilityCore;
+import com.yuanno.block_clover.api.ability.AbilityDamageKind;
+import com.yuanno.block_clover.api.ability.sorts.RepeaterAbility;
+import com.yuanno.block_clover.entities.projectiles.sword.OriginalSlashProjectile;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.play.server.SAnimateHandPacket;
+import net.minecraft.world.server.ServerWorld;
+
+public class OriginalSlashesAbility extends RepeaterAbility {
+
+    public static final AbilityCore INSTANCE = new AbilityCore.Builder("Original slashes", AbilityCategories.AbilityCategory.ATTRIBUTE, OriginalSlashesAbility.class)
+            .setDescription("Shoots a big amount of slashes made by your sword")
+            .setDamageKind(AbilityDamageKind.ELEMENTAL)
+            .setDependencies(MagicDwellerCleanAbility.INSTANCE)
+            .build();
+
+    public OriginalSlashesAbility()
+    {
+        super(INSTANCE);
+        this.setMaxCooldown(10);
+        this.setmanaCost(20);
+        this.setMaxRepeaterCount(15, 4);
+        this.setExperiencePoint(20);
+        this.setExperienceGainLevelCap(20);
+
+        this.onUseEvent = this::onUseEvent;
+    }
+
+    private boolean onUseEvent(PlayerEntity player)
+    {
+        OriginalSlashProjectile projectile = new OriginalSlashProjectile(player.level, player);
+        player.level.addFreshEntity(projectile);
+        ((ServerWorld) player.level).getChunkSource().broadcastAndSend(player, new SAnimateHandPacket(player, 0));
+        projectile.shootFromRotation(player, player.xRot, player.yRot, 0, 3, 20);
+
+        return true;
+    }
+}

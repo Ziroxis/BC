f5e77a9d51d162562c64fe66fcba63382cc68527
Beosti <ziroxismail@gmail.com>
2024-02-25 19:06:01 +0100

    ye this ability sucks ass

--- src/main/java/com/yuanno/block_clover/spells/water/SeaDragonsNestAbility.java
+++ src/main/java/com/yuanno/block_clover/spells/water/SeaDragonsNestAbility.java
@@ -1,5 +1,6 @@
 package com.yuanno.block_clover.spells.water;
 
+import com.yuanno.block_clover.api.Beapi;
 import com.yuanno.block_clover.api.ability.AbilityCategories;
 import com.yuanno.block_clover.api.ability.AbilityCore;
 import com.yuanno.block_clover.api.ability.sorts.ContinuousAbility;
@@ -18,6 +19,7 @@ public class SeaDragonsNestAbility extends ContinuousAbility {
     public SeaDragonsNestAbility() {
         super(INSTANCE);
         this.duringContinuityEvent = this::duringSpell;
+        this.onEndContinuityEvent = this::onEndContinuityEvent;
     }
 
     private void duringSpell(PlayerEntity player, int timer) {
@@ -25,29 +27,19 @@ public class SeaDragonsNestAbility extends ContinuousAbility {
         int playerY = (int) player.getY();
         int playerZ = (int) player.getZ();
 
-        for (int x = playerX - 3; x <= playerX + 1; x++) {
-            for (int y = playerY - 3; y <= playerY + 3; y++) {
-                for (int z = playerZ - 3; z <= playerZ + 1; z++) {
-                    // Skip the blocks in a 2-block radius around the player
-                    if (Math.abs(x - playerX) <= 2 && Math.abs(y - playerY) <= 2 && Math.abs(z - playerZ) <= 2) {
-                        continue;
-                    }
 
-                    // Get the block at the current position
-                    BlockPos pos = new BlockPos(x, y, z);
-                    BlockState state = player.level.getBlockState(pos);
+    }
 
-                    // If the block is air, replace it with water
-                    if (state.isAir()) {
-                        player.level.setBlock(pos, Blocks.WATER.defaultBlockState(), 3);
-                    }
-                }
-            }
-        }
+    private boolean onEndContinuityEvent(PlayerEntity player)
+    {
+        int playerX = (int) player.getX();
+        int playerY = (int) player.getY();
+        int playerZ = (int) player.getZ();
 
-        for (int x = playerX - 2; x <= playerX + 0; x++) {
-            for (int y = playerY - 2; y <= playerY + 2; y++) {
-                for (int z = playerZ - 1; z <= playerZ + 1; z++) {
+
+        for (int x = playerX - 2; x <= playerX + 4; x++) {
+            for (int y = playerY - 2; y <= playerY + 4; y++) {
+                for (int z = playerZ - 1; z <= playerZ + 4; z++) {
                     BlockPos pos = new BlockPos(x, y, z);
                     BlockState blockState = player.level.getBlockState(pos);
                     if (blockState.getBlock() == Blocks.WATER) {
@@ -57,5 +49,6 @@ public class SeaDragonsNestAbility extends ContinuousAbility {
             }
         }
 
+        return true;
     }
 }

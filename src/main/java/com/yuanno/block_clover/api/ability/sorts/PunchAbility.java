64849b9cedda04c91331a05592f8ddef0d94eed1
Beosti <ziroxismail@gmail.com>
2024-03-02 01:43:02 +0100

    ome more comments

--- src/main/java/com/yuanno/block_clover/api/ability/sorts/PunchAbility.java
+++ src/main/java/com/yuanno/block_clover/api/ability/sorts/PunchAbility.java
@@ -24,7 +24,7 @@ import java.io.Serializable;
  * 2. {@link #tick(PlayerEntity)} what happens every tick of the ability
  */
 public class PunchAbility extends ContinuousAbility {
-    private boolean isStoppingAfterHit = true;s
+    private boolean isStoppingAfterHit = true;
     protected IOnHitEntity onHitEntityEvent = (player, target) -> { return 0; };
     protected IOnHitEffect onHitEffectEvent = (player, target) -> {};
 
